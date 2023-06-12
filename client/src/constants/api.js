adc6f0b815e61fc25d758f7818ca0b77991cb498
almoghr <almoghr36@gmail.com>
2023-06-12 20:15:31 +0300

    another fix

--- client/src/constants/api.js
+++ client/src/constants/api.js
@@ -1,4 +1,4 @@
-export const GET_ALL_PRODUCTS_PATH = process.env.NODE_ENV === "development" ? "http://localhost:8001/products" : "https://gocodeshopfebfullproject.onrender.com/"
+export const GET_ALL_PRODUCTS_PATH = process.env.NODE_ENV === "development" ? "http://localhost:8001/products" : "https://gocodeshopfebfullproject.onrender.com/products"
 export const GET_PRODUCT_BY_ID = process.env.NODE_ENV === "development" ? "http://localhost:8001/product/" : "https://gocodeshopfebfullproject.onrender.com/product"
 // export const
 // export const
